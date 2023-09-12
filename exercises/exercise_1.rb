28f6b394374ef61920e3e2dbb13ec086bdb31caa
myfootsasleep <richardoda@gmail.com>
2023-09-12 00:29:47 -0700

    Completed the first two execerises. added puts to ensure that they are working correctly

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: "1260000 ", womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: "190000  ", mens_apparel: true)
+
+store_count = Store.count
+
+puts "Number of stores in the database: #{store_count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@stores1 = Store.find_by(id: 1 )
+@stores1.name = "Yaletown"
+@stores1.save
+@stores2 = Store.find_by(id: 2 )
+
+puts @stores1.name
