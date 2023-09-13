2624455c11bb6c9656f45faea31d8664b41ad98b
myfootsasleep <richardoda@gmail.com>
2023-09-12 21:17:16 -0700

    Completed and tested exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_sum = Store.sum(:annual_revenue)
+puts total_sum
+average_sum = Store.sum(:annual_revenue)/Store.count()
+puts average_sum
+stores_over_1m = Store.where('annual_revenue >= ?', 1_000_000).count
+puts stores_over_1m
